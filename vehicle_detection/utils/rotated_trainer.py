cc66fce17969b3f829e7b2dd562af5233d5f9821
Marcel Sheeny <marcelsheeny@gmail.com>
2020-12-01 09:10:42 +0000

    adding rotated_trainer

---
+++ vehicle_detection/utils/rotated_trainer.py
@@ -0,0 +1,49 @@
+import numpy as np
+import copy
+import torch
+
+from detectron2.structures import BoxMode
+from detectron2.data import transforms as T
+from detectron2.data import detection_utils as utils
+from detectron2.data import build_detection_train_loader
+from detectron2.engine import DefaultTrainer
+
+
+def transform_instance_annotations(annotation, transforms, image_size, *, keypoint_hflip_indices=None):
+    if annotation["bbox_mode"] == BoxMode.XYWHA_ABS:
+        annotation["bbox"] = transforms.apply_rotated_box(
+            np.asarray([annotation["bbox"]]))[0]
+    else:
+        bbox = BoxMode.convert(
+            annotation["bbox"], annotation["bbox_mode"], BoxMode.XYXY_ABS)
+        # Note that bbox is 1d (per-instance bounding box)
+        annotation["bbox"] = transforms.apply_box([bbox])[0]
+        annotation["bbox_mode"] = BoxMode.XYXY_ABS
+
+    return annotation
+
+
+def mapper(dataset_dict):
+    # Implement a mapper, similar to the default DatasetMapper, but with our own customizations
+    # it will be modified by code below
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    image, transforms = T.apply_transform_gens([T.Resize((1152, 1152))], image)
+    dataset_dict["image"] = torch.as_tensor(
+        image.transpose(2, 0, 1).astype("float32"))
+
+    annos = [
+        transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances_rotated(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+
+class RotatedTrainer(DefaultTrainer):
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=mapper)
--- vehicle_detection/utils/trainer.py
+++ vehicle_detection/utils/trainer.py
@@ -20,10 +20,7 @@ import logging
 import os
 from collections import OrderedDict
 
-import detectron2.utils.comm as comm
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.engine import DefaultTrainer
 from detectron2.modeling import GeneralizedRCNNWithTTA
 
 
@@ -52,58 +49,3 @@ class Trainer(DefaultTrainer):
         res = cls.test(cfg, model, evaluators)
         res = OrderedDict({k + "_TTA": v for k, v in res.items()})
         return res
-
-
-def setup(args):
-    """
-    Create configs and perform basic setups.
-    """
-    cfg = get_cfg()
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
-    cfg.freeze()
-    default_setup(cfg, args)
-    return cfg
-
-
-def main(args):
-    cfg = setup(args)
-
-    if args.eval_only:
-        model = Trainer.build_model(cfg)
-        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-            cfg.MODEL.WEIGHTS, resume=args.resume
-        )
-        res = Trainer.test(cfg, model)
-        if cfg.TEST.AUG.ENABLED:
-            res.update(Trainer.test_with_TTA(cfg, model))
-        if comm.is_main_process():
-            verify_results(cfg, res)
-        return res
-
-    """
-    If you'd like to do anything fancier than the standard training logic,
-    consider writing your own training loop (see plain_train_net.py) or
-    subclassing the trainer.
-    """
-    trainer = Trainer(cfg)
-    trainer.resume_or_load(resume=args.resume)
-    if cfg.TEST.AUG.ENABLED:
-        trainer.register_hooks(
-            [hooks.EvalHook(
-                0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
-        )
-    return trainer.train()
-
-
-if __name__ == "__main__":
-    args = default_argument_parser().parse_args()
-    print("Command Line Args:", args)
-    launch(
-        main,
-        args.num_gpus,
-        num_machines=args.num_machines,
-        machine_rank=args.machine_rank,
-        dist_url=args.dist_url,
-        args=(args,),
-    )
